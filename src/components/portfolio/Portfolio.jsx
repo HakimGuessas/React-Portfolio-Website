6a78683b234b5ede15cb8c4abc6d0ee82146a0e8
Your Name <you@example.com>
2023-02-11 11:55:51 +0100

    the proble solved and the images show

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import "./Portfolio.css"
 import img1 from "../../assets/portfolio1.jpg"
+import img2 from "../../assets/portfolio2.jpg"
+import img3 from "../../assets/portfolio3.jpg"
+import img4 from "../../assets/portfolio4.jpg"
+import img5 from "../../assets/portfolio5.png"
+import img6 from "../../assets/portfolio6.jpg"
 
 
 const Portfolio = () => {
